326af5934b83b7679f75f14921d077e2162ff033
Vladimir Ivanenko <insgarant@mail.ru>
2011-10-21 17:32:59 +0400

    astrobot_012 Опыты с ROI.

--- frameproc.cpp
+++ frameproc.cpp
@@ -6,6 +6,9 @@ void FrameProc::FrameProcSlot(void *pFrame,
 {
     cv::Mat cvMat(frameHeight, frameWidth, CV_8UC1, pFrame);
     cv::blur(cvMat, cvMat, cv::Size(5,5));
+    cv::Mat roi(cvMat, cv::Rect(100, 100, 300, 300));
+    cv::namedWindow("roi");
+    cv::imshow("roi", roi);
     emit DrawFrameSignal(pFrame, frameWidth, frameHeight);
 }
 
